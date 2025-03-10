03544b3022087fbed6d11d9a24a88f176c4a54d9
not-elm <elmgameinfo@gmail.com>
2025-03-10 23:09:40 +0900

    fix: retarget bones
    Modify the retargeting so that rotation uses local rotation, and translation affects only the position of the hips.

--- src/vrm/humanoid_bone.rs
+++ src/vrm/humanoid_bone.rs
@@ -1,15 +1,18 @@
 use crate::system_param::child_searcher::ChildSearcher;
 use crate::vrm::extensions::VrmNode;
-use crate::vrm::{BonePgRestQuaternion, BoneRestTransform, VrmBone, VrmHipsBoneTo};
+use crate::vrm::{BoneRestGlobalTransform, BoneRestTransform, VrmBone, VrmHipsBoneTo};
 use bevy::app::{App, Plugin, Update};
 use bevy::asset::{Assets, Handle};
 use bevy::core::Name;
 use bevy::gltf::GltfNode;
 use bevy::hierarchy::Children;
-use bevy::math::Quat;
-use bevy::prelude::{Added, Commands, Component, Deref, Entity, Query, Reflect, Transform};
+use bevy::prelude::*;
 use bevy::utils::HashMap;
+use serde::{Deserialize, Serialize};
 
+#[derive(Component, Reflect, Debug, Clone, Eq, PartialEq, Hash, Serialize, Deserialize)]
+#[reflect(Component, Serialize, Deserialize)]
+pub struct Hips;
 
 #[derive(Component, Deref, Reflect)]
 pub struct HumanoidBoneRegistry(HashMap<VrmBone, Name>);
@@ -38,6 +41,7 @@ impl Plugin for VrmHumanoidBonePlugin {
     fn build(&self, app: &mut App) {
         app
             .register_type::<HumanoidBoneRegistry>()
+            .register_type::<Hips>()
             .add_systems(Update, attach_bones);
     }
 }
@@ -46,45 +50,26 @@ fn attach_bones(
     mut commands: Commands,
     searcher: ChildSearcher,
     mascots: Query<(Entity, &HumanoidBoneRegistry), Added<Children>>,
-    transforms: Query<(&Transform, Option<&Children>)>,
+    transforms: Query<(&Transform, &GlobalTransform)>,
 ) {
     for (mascot_entity, humanoid_bones) in mascots.iter() {
         for (bone, name) in humanoid_bones.iter() {
             let Some(bone_entity) = searcher.find_from_name(mascot_entity, name.as_str()) else {
                 continue;
             };
-            commands.entity(bone_entity).insert(bone.clone());
-            // Use hips when sitting on window.
+            let Ok((tf, gtf)) = transforms.get(bone_entity) else {
+                continue;
+            };
+            commands.entity(bone_entity).insert((
+                bone.clone(),
+                BoneRestTransform(*tf),
+                BoneRestGlobalTransform(*gtf),
+            ));
+            // Use hips when sitting on window and retargeting.
             if bone.0 == "hips" {
                 commands.entity(mascot_entity).insert(VrmHipsBoneTo(bone_entity));
-                recursive_attach_pg_resets(
-                    &mut commands,
-                    bone_entity,
-                    Quat::IDENTITY,
-                    &transforms,
-                );
+                commands.entity(bone_entity).insert(Hips);
             }
-        }
-    }
-}
-
-fn recursive_attach_pg_resets(
-    commands: &mut Commands,
-    bone_entity: Entity,
-    rest: Quat,
-    transforms: &Query<(&Transform, Option<&Children>)>,
-) {
-    let Ok((tf, children)) = transforms.get(bone_entity) else {
-        return;
-    };
-    let pg_rest = BonePgRestQuaternion(tf.rotation * rest);
-    commands.entity(bone_entity).insert((
-        BoneRestTransform(*tf),
-        pg_rest,
-    ));
-    if let Some(children) = children {
-        for child in children.iter() {
-            recursive_attach_pg_resets(commands, *child, pg_rest.0, transforms);
         }
     }
 }
