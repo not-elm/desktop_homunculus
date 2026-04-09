778435440e7db7c7955e372b921e59ad3a65aa96
not-elm <elmgameinfo@gmail.com>
2026-04-09 14:17:32 +0900

    adjust offset and size

--- mods/persona/commands/open-ui.ts
+++ mods/persona/commands/open-ui.ts
@@ -7,12 +7,14 @@ import { audio, Webview, webviewSource } from "@hmcs/sdk";
 import { input, output } from "@hmcs/sdk/commands";
 
 try {
-  const { linkedPersona: personaId } = await input.parse(z.object({ linkedPersona: z.string() }));
+  const { linkedPersona: personaId } = await input.parse(
+    z.object({ linkedPersona: z.string() }),
+  );
   await Webview.open({
     source: webviewSource.local("persona:ui"),
-    size: [1.4, 0.85],
-    viewportSize: [1200, 700],
-    offset: [1.3, 0],
+    size: [1, 0.85],
+    viewportSize: [1000, 700],
+    offset: [1.4, -0.5],
     linkedPersona: personaId,
   });
   await audio.se.play("se:open");
